1692a0d73dd3bd41b8614ddb767181c64c2a3b16
Yiyu Zhou <yiyuzhou19@gmail.com>
2023-11-18 01:30:27 -0800

    Improve question detection

---
+++ src/question.py
@@ -0,0 +1,35 @@
+import re
+
+from typing import List
+
+
+# Splits a sentence into segments. For example, "Hi, what's hash table?" would
+# turn into ["Hi" "what's hash table"]
+def parse(sentence: str) -> List[str]:
+    segments: List[str] = re.split(r"[,.?!]+", sentence)
+    segments = [segment.strip() for segment in segments if segment.strip()]
+    return segments
+
+
+def is_question(question: str) -> bool:
+    indicators: List[str] = [
+        "why",
+        "what",
+        "who",
+        "whose",
+        "which",
+        "when",
+        "where",
+        "how",
+    ]
+
+    ret: bool = False
+    if "?" in question.lower():
+        ret = True
+    else:
+        for segment in parse(question.lower()):
+            for word in indicators:
+                if segment.split()[0] == word:
+                    ret = True
+
+    return ret
