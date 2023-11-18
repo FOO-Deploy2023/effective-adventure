dfcffda161df23855a85a1cf8ce9f36ed837fe9d
Tao Tien <29749622+taotien@users.noreply.github.com>
2023-11-17 20:14:57 -0800

    rebase

--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
-import sys
 import os
+import sys
+import discord
 
 
-def get_env(name: str) -> str:
-    ENV = os.getenv(name)
-    return ENV
+class TestClient(discord.Client):
+    async def on_ready(self):
+        print("Logged on as", self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == "ping":
+            await message.channel.send("pong")
 
 
-def main() -> int:
-    print(get_env("DISCORD_TOKEN"))
-    return 0
+def main():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = TestClient(intents=intents)
+    client.run(os.environ["DISCORD_TOKEN"])
 
 
 if __name__ == "__main__":
