8bfadfa49f031298015df2a5d9ebdd061c8efb58
Tao Tien <29749622+taotien@users.noreply.github.com>
2023-11-17 20:14:57 -0800

    environment and ping

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
+import os
 import sys
+import discord
 
 
-def main() -> int:
-    print("hello world!")
-    return 0
+class TestClient(discord.Client):
+    async def on_ready(self):
+        print("Logged on as", self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == "ping":
+            await message.channel.send("pong")
+
+
+def main():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = TestClient(intents=intents)
+    client.run(os.environ["DISCORD_TOKEN"])
 
 
 if __name__ == "__main__":
