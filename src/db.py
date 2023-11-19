3014b13eb2247ee9582bd51049c7ea3b05382600
Yiyu Zhou <yiyuzhou19@gmail.com>
2023-11-18 23:20:22 -0800

    Clean up format

--- src/db.py
+++ src/db.py
@@ -2,11 +2,11 @@ import pickle
 
 
 def store(data):
-    with open('data.pkl', 'wb') as file:
+    with open("data.pkl", "wb") as file:
         pickle.dump(data, file)
 
 
 def restore():
-    with open('data.pkl', 'rb') as file:
+    with open("data.pkl", "rb") as file:
         loaded_data = pickle.load(file)
         return loaded_data
