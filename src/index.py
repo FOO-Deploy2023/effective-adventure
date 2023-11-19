50be900f995a5fd3d1b905b2dddf944d57227b52
Andrew <adiep@dons.usfca.edu>
2023-11-18 17:22:02 -0800

    Reverse key and value for inner dict

--- src/index.py
+++ src/index.py
@@ -1,6 +1,5 @@
 import yake
 
-
 def extract(msg: str, table):
     kw_extractor = yake.KeywordExtractor()
     keywords = kw_extractor.extract_keywords(msg)
@@ -9,8 +8,8 @@ def extract(msg: str, table):
     for kw in keywords:
         keyphrase, score = kw
 
-        treemap_val = table[keyphrase]
-        treemap_val[score] = msg
+        inner_table = table[keyphrase]
+        inner_table[msg] = score
 
         print(kw)
 
@@ -20,5 +19,10 @@ def print_dict(table):
     print("\nComplete Table View:")
     for keyphrase, treemap in table.items():
         print(f"Keyphrase: '{keyphrase}'")
-        for score, message in treemap.items():
+
+        # Sorting the treemap based on scores (values) in descending order
+        sorted_treemap = sorted(treemap.items(), key=lambda item: item[1])
+
+        for message, score in sorted_treemap:
             print(f"  Score: {score}, Message: '{message}'")
+
